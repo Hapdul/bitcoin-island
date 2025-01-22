f7f591ca607f0d628a65a81a192bc314f42f1ee3
Mochamad Abdul Malik <mochamad.abdul.malik@gmail.com>
2025-01-22 16:51:39 +0700

    Update index.js

--- pages/what-is-bitcoin/index.js
+++ pages/what-is-bitcoin/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <h1>Sample Page</h1>
+      <h1>Bitcoin is a decentralized digital currency created in 2009 that uses blockchain technology to enable secure, peer-to-peer transactions without intermediaries, has a limited supply of 21 million coins, offers benefits like low fees and independence from government control, but faces challenges such as price volatility and regulatory uncertainty.</h1>
     </div>
   );
 };
